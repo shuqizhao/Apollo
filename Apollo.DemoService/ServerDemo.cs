36e50458133fa1d5ff4204b23787cead4b3083e0
shuqizhao <shuqizhao@beisen.com>
2017-12-12 02:08:17 +0800

    调整输出

--- Apollo.DemoService/ServerDemo.cs
+++ Apollo.DemoService/ServerDemo.cs
@@ -8,20 +8,20 @@ namespace Apollo.DemoService
     {
         public string Hello(string input)
         {
-            System.Console.WriteLine("you input is " + input);
+            System.Console.WriteLine("Hello you input is " + input);
             return "I get it，乱码？";
         }
 
         public Person GetPersonById(int id, bool isAdmin)
         {
-            System.Console.WriteLine("you input is " + id + ":" + isAdmin);
+            System.Console.WriteLine("GetPersonById you input is " + id + ":" + isAdmin);
 
             return new Person { Id = 1, Name = "xyz", Sex = "F" };
         }
 
         public void SavePerson(Person person, bool isAdmin)
         {
-            System.Console.WriteLine("you input is " + person.Id + ":" + person.Name+":"+isAdmin);
+            System.Console.WriteLine("SavePerson you input is " + person.Id + ":" + person.Name+":"+isAdmin);
 
         }
 
