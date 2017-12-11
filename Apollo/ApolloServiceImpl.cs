737ccc5d440923cae823a76c01c00fa02a497cbf
shuqizhao <shuqizhao@qq.com>
2017-12-11 20:08:41 +0800

    add try catch

--- Apollo/ApolloServiceImpl.cs
+++ Apollo/ApolloServiceImpl.cs
@@ -10,18 +10,24 @@ namespace Apollo
     {
         public override Task<Response> Call(Request request, ServerCallContext context)
         {
-            var splits = request.ServiceName.Split('$');
-            var typeName = splits[0];
-            var methodName = splits[1];
-            var type = MicroServiceManage.GetServiceType(typeName);
-            var methodType = type.GetMethod(methodName);
-            var instance = Activator.CreateInstance(type);
-            var args = JsonHelper.DeserializeJsonToObject<object[]>(request.Data);
+            try
+            {
+                var splits = request.ServiceName.Split('$');
+                var typeName = splits[0];
+                var methodName = splits[1];
+                var type = MicroServiceManage.GetServiceType(typeName);
+                var methodType = type.GetMethod(methodName);
+                var instance = Activator.CreateInstance(type);
+                var args = JsonHelper.DeserializeJsonToObject<object[]>(request.Data);
+                var result = methodType.Invoke(instance, args);
+                var resultJson = JsonConvert.SerializeObject(result);
+                return Task.FromResult(new Response { Code = "200", Data = resultJson });
+            }
+            catch (System.Exception ex)
+            {
+                return Task.FromResult(new Response { Code = "500", Data = "", Message = ex.ToString() });
+            }
 
-            var result = methodType.Invoke(instance, args);
-            //System.Console.WriteLine("I get message :"+request.ServiceName);
-            var resultJson = JsonConvert.SerializeObject(result);
-            return Task.FromResult(new Response { Data = resultJson });
         }
     }
 }
